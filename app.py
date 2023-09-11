7ba85b7c7c972bc3ade34f3c4d07032b78f1436d
aspdiscovery123 <48548207+aspdiscovery123@users.noreply.github.com>
2023-09-11 22:02:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
-from flask import Flask, render_template
-import random
+from flask import Flask
 
 app = Flask(__name__)
 
-#list of cat images
-images = [
-    "https://media.giphy.com/media/kBZBlLVlfECvOQAVno/giphy.gif","https://media.giphy.com/media/dDwicM3uFUqfC/giphy.gif","https://media.giphy.com/media/mGK1g88HZRa2FlKGbz/giphy.gif"
-]
-
 @app.route('/')
-def index():
-    url = random.choice(images)
-    return render_template('index.html', url=url)
+def hello_world():
+    return 'Hello, World!'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
